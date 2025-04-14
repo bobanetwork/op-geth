dea22cafb288b0fdc889ac65ad646ad6cf74dd9d
Michael Montour <mmontour@enya.ai>
2025-04-14 15:33:43 -0700

    feat: Define Boba chain IDs and hardfork parameters.
    Changes to be committed:
    	modified:   core/superchain.go
    	modified:   params/config.go
    	modified:   params/superchain.go
    	modified:   params/superchain_test.go

--- params/superchain_test.go
+++ params/superchain_test.go
@@ -2,7 +2,11 @@ package params
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/superchain"
+	"github.com/stretchr/testify/require"
 )
 
 type HumanProtocolVersion struct {
@@ -167,5 +171,123 @@ func TestProtocolVersion_String(t *testing.T) {
 				t.Fatalf("got %q but expected %q", got, tc.expected)
 			}
 		})
+	}
+}
+
+type hardforkConfig struct {
+	chainID                  uint64
+	ShanghaiTime             uint64
+	CancunTime               uint64
+	BedrockBlock             *big.Int
+	RegolithTime             uint64
+	CanyonTime               uint64
+	EcotoneTime              uint64
+	FjordTime                uint64
+	GraniteTime              uint64
+	HoloceneTime             uint64
+	EIP1559Elasticity        uint64
+	EIP1559Denominator       uint64
+	EIP1559DenominatorCanyon uint64
+}
+
+var bobaSepoliaCfg = hardforkConfig{
+	chainID:                  28882,
+	ShanghaiTime:             uint64(1705600788),
+	CancunTime:               uint64(1709078400),
+	BedrockBlock:             big.NewInt(511),
+	RegolithTime:             uint64(1705600788),
+	CanyonTime:               uint64(1705600788),
+	EcotoneTime:              uint64(1709078400),
+	FjordTime:                uint64(1722297600),
+	GraniteTime:              uint64(1726470000),
+	HoloceneTime:             uint64(1736150400),
+	EIP1559Elasticity:        6,
+	EIP1559Denominator:       50,
+	EIP1559DenominatorCanyon: 250,
+}
+
+var bobaMainnetCfg = hardforkConfig{
+	chainID:                  288,
+	ShanghaiTime:             uint64(1713302879),
+	CancunTime:               uint64(1713302880),
+	BedrockBlock:             big.NewInt(1149019),
+	RegolithTime:             uint64(1713302879),
+	CanyonTime:               uint64(1713302879),
+	EcotoneTime:              uint64(1713302880),
+	FjordTime:                uint64(1725951600),
+	GraniteTime:              uint64(1729753200),
+	HoloceneTime:             uint64(1738785600),
+	EIP1559Elasticity:        6,
+	EIP1559Denominator:       50,
+	EIP1559DenominatorCanyon: 250,
+}
+
+var opSepoliaCfg = hardforkConfig{
+	chainID:                  11155420,
+	ShanghaiTime:             uint64(1699981200),
+	CancunTime:               uint64(1708534800),
+	BedrockBlock:             big.NewInt(0),
+	RegolithTime:             uint64(0),
+	CanyonTime:               uint64(1699981200),
+	EcotoneTime:              uint64(1708534800),
+	FjordTime:                uint64(1716998400),
+	GraniteTime:              uint64(1723478400),
+	HoloceneTime:             uint64(1732633200),
+	EIP1559Elasticity:        6,
+	EIP1559Denominator:       50,
+	EIP1559DenominatorCanyon: 250,
+}
+
+var opMainnetCfg = hardforkConfig{
+	chainID:                  10,
+	ShanghaiTime:             uint64(1704992401),
+	CancunTime:               uint64(1710374401),
+	BedrockBlock:             big.NewInt(105235063),
+	RegolithTime:             uint64(0),
+	CanyonTime:               uint64(1704992401),
+	EcotoneTime:              uint64(1710374401),
+	FjordTime:                uint64(1720627201),
+	GraniteTime:              uint64(1726070401),
+	HoloceneTime:             uint64(1736445601),
+	EIP1559Elasticity:        6,
+	EIP1559Denominator:       50,
+	EIP1559DenominatorCanyon: 250,
+}
+
+func TestChainConfigByOpStackChainName(t *testing.T) {
+	hardforkConfigsByName := map[uint64]hardforkConfig{
+		28882:    bobaSepoliaCfg,
+		288:      bobaMainnetCfg,
+		11155420: opSepoliaCfg,
+		10:       opMainnetCfg,
+	}
+
+	for name, expectedHarhardforkCfg := range hardforkConfigsByName {
+		chain, err := superchain.GetChain(name)
+		require.NoError(t, err)
+		chainConf, err := chain.Config()
+		require.NoError(t, err)
+		gotCfg, err := LoadOPStackChainConfig(chainConf)
+		require.NotNil(t, gotCfg)
+		require.NoError(t, err)
+
+		// ChainID
+		require.Equal(t, expectedHarhardforkCfg.chainID, gotCfg.ChainID.Uint64())
+
+		// Hardforks
+		require.Equal(t, expectedHarhardforkCfg.ShanghaiTime, *gotCfg.ShanghaiTime)
+		require.Equal(t, expectedHarhardforkCfg.CancunTime, *gotCfg.CancunTime)
+		require.Equal(t, expectedHarhardforkCfg.BedrockBlock, gotCfg.BedrockBlock)
+		require.Equal(t, expectedHarhardforkCfg.RegolithTime, *gotCfg.RegolithTime)
+		require.Equal(t, expectedHarhardforkCfg.CanyonTime, *gotCfg.CanyonTime)
+		require.Equal(t, expectedHarhardforkCfg.EcotoneTime, *gotCfg.EcotoneTime)
+		require.Equal(t, expectedHarhardforkCfg.FjordTime, *gotCfg.FjordTime)
+		require.Equal(t, expectedHarhardforkCfg.GraniteTime, *gotCfg.GraniteTime)
+		require.Equal(t, expectedHarhardforkCfg.HoloceneTime, *gotCfg.HoloceneTime)
+
+		// EIP-1559
+		require.Equal(t, expectedHarhardforkCfg.EIP1559Elasticity, gotCfg.Optimism.EIP1559Elasticity)
+		require.Equal(t, expectedHarhardforkCfg.EIP1559Denominator, gotCfg.Optimism.EIP1559Denominator)
+		require.Equal(t, expectedHarhardforkCfg.EIP1559DenominatorCanyon, *gotCfg.Optimism.EIP1559DenominatorCanyon)
 	}
 }
